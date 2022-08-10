ed37e9c4b0d27ee9ca9affe2472688f997fde257
Luis Teyssier <luisteyssier@MacBook-Pro-de-Luis.local>
2022-08-10 14:16:29 -0500

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1fd2751a0dbc052386043ab51389ff61'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '93c27d9f-c32c-427e-89e0-d7cdb773165e'
+};
